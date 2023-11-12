883939ae73fa581e444aad0594617ca8b768db2f
Noalino <bgelineau.dev@gmail.com>
2023-11-12 11:46:46 +0100

    Complete challenge

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -7,15 +7,125 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct FlagImage: View {
+    let name: String
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Image(name)
+            .clipShape(.capsule)
+            .shadow(radius: 5)
+    }
+}
+
+struct ContentView: View {
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var score = 0
+    @State private var guessCount = 0
+
+    @State private var showingScore = false
+    @State private var showingFinalScore = false
+    @State private var scoreTitle = ""
+
+    @State private var tappedFlag = ""
+
+    var body: some View {
+        ZStack {
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.2), location: 0.3),
+            ], center: .top, startRadius: 200, endRadius: 700)
+                .ignoresSafeArea()
+
+            VStack {
+                Spacer()
+
+                Text("Guess the Flag")
+                    .font(.largeTitle.bold())
+                    .foregroundStyle(.white)
+
+                VStack(spacing: 30) {
+                    VStack {
+                        Text("Tap the flag of")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold))
+                    }
+
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            FlagImage(name: countries[number])
+                        }
+                        .rotation3DEffect(
+                            .degrees(countries[number] == tappedFlag ? 360 : 0),
+                            axis: (x: 0.0, y: 1.0, z: 0.0)
+                        )
+                        .animation(tappedFlag.isEmpty ? nil : .default, value: tappedFlag)
+                        .opacity((!tappedFlag.isEmpty && countries[number] != tappedFlag) ? 0.25 : 1)
+                        .scaleEffect((!tappedFlag.isEmpty && countries[number] != tappedFlag) ? 0.75 : 1)
+                        .animation(.easeOut, value: tappedFlag)
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+                .clipShape(.rect(cornerRadius: 20))
+
+                Spacer()
+                Spacer()
+
+                Text("Score: \(score)")
+                    .font(.title.bold())
+                    .foregroundStyle(.white)
+
+                Spacer()
+            }
+            .padding()
         }
-        .padding()
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your score is \(score)")
+        }
+        .alert("Game over", isPresented: $showingFinalScore) {
+            Button("Restart", action: resetGame)
+        } message: {
+            Text("Your final score is \(score)")
+        }
+    }
+
+    func flagTapped(_ number: Int) {
+        tappedFlag = countries[number]
+        guessCount += 1
+
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+            score += 1
+        } else {
+            scoreTitle = "Wrong, that's the flag of \(countries[number])"
+        }
+
+        if guessCount == 8 {
+            showingFinalScore = true
+        } else {
+            showingScore = true
+        }
+    }
+
+    func askQuestion() {
+        tappedFlag = ""
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+
+    func resetGame() {
+        score = 0
+        guessCount = 0
+        askQuestion()
     }
 }
 
